50c272ca5cb53a81742cff9e302da0c6a6c4edc7
Manav Misra <manavm@visionwebsoft.com>
2020-08-29 19:41:10 -0500

    Fix minor spacing in markup text

--- src/components/views/Home.js
+++ src/components/views/Home.js
@@ -12,33 +12,33 @@ export const Home = () => (
       </span>
     </p>
     <p>
-      Player 1 (left) will use the &quot;A&quot;
+      Player 1 (left) will use the &quot;A&quot; &nbsp;
       <span role="img" aria-label="key">
-        🔑
+        🔑,
       </span>
-      , and Player 2 (right) will use the &quot;L&quot;
+      &nbsp; and Player 2 (right) will use the &quot;L&quot; &nbsp;
       <span role="img" aria-label="key">
-        🔑
+        🔑.
       </span>
-      .
+      &nbsp;
     </p>
 
     <p>
       Once the game starts, a card will be shown every second.{" "}
       <strong>
-        If that card{" "}
+        If that card&nbsp;
         <span role="img" aria-label="flower playing cards">
           🎴
         </span>
-        is a &quot;Jack,&quot; dealing will stop and u should hit ur button
-        ASAP!
+        &nbsp; is a &quot;Jack,&quot; dealing will stop and u should hit ur
+        button ASAP!
       </strong>
     </p>
     <p>The first player to press their button will get the point.</p>
     <p>Then, cards will continue being dealt.</p>
     <p>
       The game will continue until someone scores &quot;3,&quot; or until we are
-      out of Jacks; in this case, there would be a 2-2 tie{" "}
+      out of Jacks; in this case, there would be a 2-2 tie&nbsp;
       <span role="img" aria-label="shrug">
         🤷🏽‍♂️
       </span>
